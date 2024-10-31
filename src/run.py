4af73de8e142af9fc5171be768b36922390e14ba
joanca <jcaravenae@gmail.com>
2024-10-31 01:51:48 +0100

    adding run.py script to run the problem

---
+++ src/run.py
@@ -0,0 +1,22 @@
+from qiskit import QuantumCircuit
+
+from noise import add_pauli_noise
+from decompose_basis import decompose_into_basis
+from quantum_sum import quantum_sum
+
+circuit = QuantumCircuit(2)
+
+circuit.h(1)
+
+circuit.z(0)
+print(circuit)
+
+noisy_circuit = add_pauli_noise(circuit, 0.1, 0.8)
+
+print(noisy_circuit)
+
+decomposed_circuit = decompose_into_basis(circuit)
+
+print(decomposed_circuit)
+
+quantum_sum = quantum_sum(1, 3)
